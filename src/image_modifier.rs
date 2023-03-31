6e4cced1ab80866cc7002ddfda07ac82c9383683
Edwin Svensson <git@olback.net>
2023-03-31 18:24:43 +0200

    Initial commit

---
+++ src/image_modifier.rs
@@ -0,0 +1,110 @@
+use {
+    fast_image_resize as fr,
+    fastblur::gaussian_blur,
+    image::{imageops, RgbImage},
+    std::num::NonZeroU32,
+};
+
+pub fn image_background(
+    background: &RgbImage,
+    display_geometry: [u32; 2],
+    blur_radius: Option<u32>,
+) -> RgbImage {
+    if let Some(blur) = blur_radius {
+        add_blur(
+            background,
+            display_geometry[0],
+            display_geometry[1],
+            blur as f32,
+        )
+    } else {
+        fast_resize(background, display_geometry[0], display_geometry[1])
+    }
+}
+
+fn add_blur(image: &RgbImage, nwidth: u32, nheight: u32, blur_radius: f32) -> RgbImage {
+    // Downsize the image by a factor of `scale` for faster blurring and upscale back
+    // to display_geometry for final image
+
+    let scale = 4;
+    let (scaled_width, scaled_height) = (nwidth / scale, nheight / scale);
+    let downscaled_image = fast_resize(image, scaled_width, scaled_height);
+
+    let mut pixels: Vec<[u8; 3]> = downscaled_image
+        .into_raw()
+        .chunks_exact(3)
+        .map(|pixel| pixel.try_into().unwrap())
+        .collect();
+
+    gaussian_blur(
+        &mut pixels,
+        scaled_width as usize,
+        scaled_height as usize,
+        blur_radius / scale as f32,
+    );
+
+    let buf = pixels.into_iter().flatten().collect();
+    let blurred_image = RgbImage::from_raw(scaled_width, scaled_height, buf).unwrap();
+    fast_resize(&blurred_image, nwidth, nheight)
+}
+
+pub fn fast_resize(img: &RgbImage, nwidth: u32, nheight: u32) -> RgbImage {
+    let width = NonZeroU32::new(img.width()).unwrap();
+    let height = NonZeroU32::new(img.height()).unwrap();
+    let src_image =
+        fr::Image::from_vec_u8(width, height, img.as_raw().to_vec(), fr::PixelType::U8x3).unwrap();
+    let resized_image = resize_image_with_cropping(
+        src_image.view(),
+        NonZeroU32::new(nwidth).unwrap(),
+        NonZeroU32::new(nheight).unwrap(),
+    );
+    RgbImage::from_raw(nwidth, nheight, resized_image.into_vec()).unwrap()
+}
+
+fn resize_image_with_cropping(
+    mut src_view: fr::ImageView,
+    dst_width: NonZeroU32,
+    dst_height: NonZeroU32,
+) -> fr::Image {
+    // Set cropping parameters
+    src_view.set_crop_box_to_fit_dst_size(dst_width, dst_height, None);
+
+    // Create container for data of destination image
+    let mut dst_image = fr::Image::new(dst_width, dst_height, src_view.pixel_type());
+    // Get mutable view of destination image data
+    let mut dst_view = dst_image.view_mut();
+
+    // Create Resizer instance and resize source image
+    // into buffer of destination image
+    let mut resizer = fr::Resizer::new(fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3));
+    resizer.resize(&src_view, &mut dst_view).unwrap();
+
+    dst_image
+}
+
+pub fn paste_images(
+    background: &RgbImage,
+    foreground: &RgbImage,
+    display_geometry: [u32; 2],
+    available_geometry: [u32; 4],
+) -> RgbImage {
+    let mut base = RgbImage::new(display_geometry[0], display_geometry[1]);
+
+    // Background Paste
+    let x = (display_geometry[0] as i64 - background.width() as i64) / 2;
+    let y = (display_geometry[1] as i64 - background.height() as i64) / 2;
+
+    imageops::overlay(&mut base, background, x, y);
+
+    // Foreground paste
+
+    let x = (i64::from(available_geometry[0]) - i64::from(foreground.width())) / 2
+        + i64::from(available_geometry[2]);
+
+    let y = (i64::from(available_geometry[1]) - i64::from(foreground.height())) / 2
+        + i64::from(available_geometry[3]);
+
+    imageops::overlay(&mut base, foreground, x, y);
+
+    base
+}
---
+++ src/main.rs
@@ -0,0 +1,145 @@
+// Most of the code used here is borrowed from
+// https://github.com/diwic/dbus-rs/
+// https://github.com/jac0b-w/AlbumPaper/tree/master
+// https://github.com/l1na-forever/mpris-notifier/tree/mainline
+
+use {
+    dbus::{blocking::Connection, channel::MatchingReceiver, message::MatchRule, Message},
+    giftwrap::Wrap,
+    mpris::PlayerFinder,
+    reqwest::blocking::get,
+    std::{io::Cursor, time::Duration},
+};
+
+mod image_modifier;
+
+#[derive(Debug, Wrap)]
+enum Error {
+    Reqwest(reqwest::Error),
+    String(String),
+    DBus(mpris::DBusError),
+    FindingError(mpris::FindingError),
+    Image(image::error::ImageError),
+    Io(std::io::Error),
+}
+
+const DEFAULT_WALLPAPER: &str = "/usr/share/backgrounds/Alma-mountains-dark.xml";
+const GENERATED_WALLPAPER: &str = "/tmp/cover-art.png";
+const WIDTH: u32 = 2560;
+const HEIGHT: u32 = 1440;
+
+// const MPRIS_INTERFACE: &str = "org.mpris.MediaPlayer2.Player";
+const MPRIS_SIGNAL_INTERFACE: &str = "org.freedesktop.DBus.Properties";
+const MPRIS_SIGNAL_MEMBER: &str = "PropertiesChanged";
+const MPRIS_SIGNAL_OBJECT: &str = "/org/mpris/MediaPlayer2";
+
+fn main() {
+    // First open up a connection to the session bus.
+    let conn = Connection::new_session().expect("D-Bus connection failed");
+
+    // Second create a rule to match messages we want to receive; in this example we add no
+    // further requirements, so all messages will match
+    let mut rule = MatchRule::new();
+    rule.interface = Some(dbus::strings::Interface::new(MPRIS_SIGNAL_INTERFACE).unwrap());
+    rule.member = Some(dbus::strings::Member::new(MPRIS_SIGNAL_MEMBER).unwrap());
+    rule.path = Some(dbus::strings::Path::new(MPRIS_SIGNAL_OBJECT).unwrap());
+
+    // Try matching using new scheme
+    let proxy = conn.with_proxy(
+        "org.freedesktop.DBus",
+        "/org/freedesktop/DBus",
+        Duration::from_millis(5000),
+    );
+    let result: Result<(), dbus::Error> = proxy.method_call(
+        "org.freedesktop.DBus.Monitoring",
+        "BecomeMonitor",
+        (vec![rule.match_str()], 0u32),
+    );
+
+    if result.is_ok() {
+        // Start matching using new scheme
+        conn.start_receive(
+            rule,
+            Box::new(|msg, _| {
+                handle_message(&msg);
+                true
+            }),
+        );
+    } else {
+        // Start matching using old scheme
+        rule.eavesdrop = true; // this lets us eavesdrop on *all* session messages, not just ours
+        conn.add_match(rule, |_: (), _, msg| {
+            handle_message(msg);
+            true
+        })
+        .expect("add_match failed");
+    }
+
+    loop {
+        conn.process(Duration::from_millis(1000)).unwrap();
+    }
+}
+
+fn handle_message(_: &Message) {
+    let set_result = match gen_wallpaper() {
+        Ok(_) => dconf_rs::set_string(
+            "/org/gnome/desktop/background/picture-uri",
+            &format!("file://{GENERATED_WALLPAPER}"),
+        ),
+        Err(e) => {
+            eprintln!("{e:?}");
+            dconf_rs::set_string(
+                "/org/gnome/desktop/background/picture-uri",
+                &format!("file://{DEFAULT_WALLPAPER}"),
+            )
+        }
+    };
+    match set_result {
+        Ok(_) => println!("Wallpaper set"),
+        Err(e) => eprintln!("Error: {e}"),
+    }
+}
+
+fn gen_wallpaper() -> Result<(), Error> {
+    let player = PlayerFinder::new()?.find_active()?;
+
+    let metadata = player.get_metadata()?;
+
+    let art_url = metadata
+        .art_url()
+        .ok_or_else(|| String::from("No art url found"))?;
+    println!("Art url: {art_url}");
+
+    let art_bytes = if art_url.starts_with("http") {
+        let art_response = get(art_url)?;
+        let art_download_status = art_response.status();
+        if !art_download_status.is_success() {
+            panic!(
+                "Got {} {} while downloading art",
+                art_download_status.as_u16(),
+                art_download_status.as_str()
+            );
+        }
+
+        art_response.bytes()?.to_vec()
+    } else {
+        std::fs::read(art_url.replace("file://", ""))?
+    };
+
+    let image_reader = image::io::Reader::new(Cursor::new(art_bytes)).with_guessed_format()?;
+    let art = image_reader.decode()?.to_rgb8();
+
+    let background = image_modifier::image_background(&art, [WIDTH, HEIGHT], Some(32));
+    let new_image = image_modifier::paste_images(
+        &background,
+        &art,
+        [WIDTH, HEIGHT],
+        [0, 0, WIDTH / 2, HEIGHT / 2],
+    );
+
+    new_image
+        .save(GENERATED_WALLPAPER)
+        .expect("Failed to save wallpaper");
+
+    Ok(())
+}
